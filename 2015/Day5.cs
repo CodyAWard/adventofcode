cf016f5d29d27ec990190b5eb5beb14cc3c86ebb
cody <codyward9@gmail.com>
2020-12-06 12:44:14 -0700

    2015 days 5 and 6

---
+++ 2015/Day5.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.MMXV
+{
+    public class Day5 : MMXVDay
+    {
+        public override string GetSolution()
+        {
+            var data = ReadData();
+            return 
+                PartA(data) + "\n" + 
+                PartB(data);
+        }
+
+        private static readonly string[] INVALID_SUBSTRINGS = new string[]
+            {
+                "ab", "cd", "pq", "xy"
+            };
+
+        private static readonly char[] VOWELS = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+        public static bool IsActuallyNice(string str)
+        {
+            int tripletIndex = -1;
+            string foundPair = null;
+
+            var secondLastC = '!';
+            var lastC = ' ';
+            for (int i = 0; i < str.Length; i++)
+            {
+                var c = str[i];
+                if (tripletIndex == -1)
+                {
+                    if (secondLastC == c) tripletIndex = i;
+                }
+
+                if (foundPair == null  && i < str.Length - 1)
+                {
+                    var pair = str.Substring(i, 2);
+                    for (int j = i + 2; j < str.Length - 1; j++)
+                    {
+                        var search = str.Substring(j, 2);
+                        if (search == pair) foundPair = search;
+                    }
+                }
+
+                secondLastC = lastC;
+                lastC = c;
+            }
+
+            if (tripletIndex == -1)
+            {
+                Console.WriteLine($"{str} NAUGHTY - no triplet");
+                return false;
+            }
+
+            if (foundPair == null)
+            {
+                Console.WriteLine($"{str} NAUGHTY - no pair");
+                return false;
+            }
+
+            Console.WriteLine($"{str} REALLY NICE - {foundPair} {str.Substring(tripletIndex - 2, 3)}");
+            return true;
+        }
+
+        public static bool IsNice(string str)
+        {
+            foreach (var invalidSub in INVALID_SUBSTRINGS)
+                if (str.Contains(invalidSub))
+                {
+                    Console.WriteLine($"{str} NAUGHTY - contains '{invalidSub}'");
+                    return false;
+                }
+
+            var vowelCount = 0;
+            var foundDouble = false;
+
+            var lastC = ' ';
+            foreach (var c in str)
+            {
+                if (c == lastC) foundDouble = true;
+                if (VOWELS.Contains(c)) vowelCount++;
+
+                lastC = c;
+            }
+
+            if (vowelCount < 3)
+            {
+                Console.WriteLine($"{str} NAUGHTY - vowels '{vowelCount}'");
+                return false; 
+            }
+
+            if (!foundDouble)
+            {
+                Console.WriteLine($"{str} NAUGHTY - no double");
+                return false;
+            }
+
+            Console.WriteLine($"{str} NICE");
+            return true;
+        }
+
+        private static string PartA(IEnumerable<string> data)
+        {
+            return data.Where(s => IsNice(s)).Count()
+                .ToString();
+        }
+
+        private static string PartB(IEnumerable<string> data)
+        {
+            return data.Where(s => IsActuallyNice(s)).Count()
+                .ToString();
+        }
+    }
+}
---
+++ 2015/Day6.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+
+namespace AdventOfCode.MMXV
+{
+    public class Day6 : MMXVDay
+    {
+        public override string GetSolution()
+        {
+            var data = ReadData();
+            return 
+                PartA(data) + "\n" + 
+                PartB(data);
+        }
+
+        enum Types
+        {
+            On, Off, Toggle,
+        }
+
+        class Instruction
+        {
+            Types Type;
+            Vector2 Min;
+            Vector2 Max;
+
+            public static Instruction Parse(string str)
+            {
+                var type = Types.Toggle;
+                if (str.StartsWith("turn on"))
+                {
+                    str = str.Replace("turn on ", string.Empty);
+                    type = Types.On;
+                }
+                else if (str.StartsWith("turn off"))
+                {
+                    str = str.Replace("turn off ", string.Empty);
+                    type = Types.Off;
+                }
+                else
+                {
+                    str = str.Replace("toggle ", string.Empty);
+                }
+
+                var rect = str.Split(" through ");
+                var min = rect[0].Split(',');
+                var max = rect[1].Split(',');
+
+                return new Instruction()
+                {
+                    Type = type,
+                    Min = new Vector2(int.Parse(min[0]), int.Parse(min[1])),
+                    Max = new Vector2(int.Parse(max[0]), int.Parse(max[1]))
+                };
+            }
+
+            public void Apply(bool[,] grid)
+            {
+                for (int x = (int)Min.X; x <= Max.X; x++)
+                    for (int y = (int)Min.Y; y <= Max.Y; y++)
+                    {
+                        switch (Type)
+                        {
+                            case Types.On:
+                                grid[x, y] = true;
+                                break;
+                            case Types.Off:
+                                grid[x, y] = false;
+                                break;
+                            case Types.Toggle:
+                                grid[x, y] = !grid[x, y];
+                                break;
+                        }
+                    }
+            }
+
+            public void Apply(int[,] grid)
+            {
+                for (int x = (int)Min.X; x <= Max.X; x++)
+                    for (int y = (int)Min.Y; y <= Max.Y; y++)
+                    {
+                        switch (Type)
+                        {
+                            case Types.On:
+                                grid[x, y] += 1;
+                                break;
+                            case Types.Off:
+                                grid[x, y] -= 1;
+                                if (grid[x, y] < 0) grid[x, y] = 0;
+                                break;
+                            case Types.Toggle:
+                                grid[x, y] += 2;
+                                break;
+                        }
+                    }
+            }
+        }
+
+        private static string PartA(IEnumerable<string> data)
+        {
+            var grid = new bool[1000, 1000];
+            foreach (var line in data)
+            {
+                Instruction.Parse(line).Apply(grid);
+            }
+
+            var on = 0;
+            for (int y = 0; y < 1000; y++)
+            for (int x = 0; x < 1000; x++)
+                {
+                    if (grid[x, y]) on++;
+                }
+
+            return $"{on} lights on";
+        }
+
+        private static string PartB(IEnumerable<string> data)
+        {
+            var grid = new int[1000, 1000];
+            foreach (var line in data)
+            {
+                Instruction.Parse(line).Apply(grid);
+            }
+
+            var bright = 0;
+            for (int y = 0; y < 1000; y++)
+            for (int x = 0; x < 1000; x++)
+                {
+                    bright += grid[x, y];
+                }
+
+            return $"{bright} lumens";
+        }
+    }
+}
